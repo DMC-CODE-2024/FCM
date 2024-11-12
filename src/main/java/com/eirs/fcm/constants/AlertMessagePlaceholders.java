9edd4256c6c68f071d564b76d32abe14326191d6
Arun Mehta <arun271333@gmail.com>
2024-11-12 19:26:59 +0530

    Alert Parameters Changes

--- src/main/java/com/eirs/fcm/constants/AlertMessagePlaceholders.java
+++ src/main/java/com/eirs/fcm/constants/AlertMessagePlaceholders.java
@@ -1,8 +1,13 @@
 package com.eirs.fcm.constants;
 
 public enum AlertMessagePlaceholders {
-    URL("<URL>"), LIST("<LIST>"), EXCEPTION("<EXCEPTION>"), FILE_TYPE("<FILE_TYPE>"),
-    CONFIG_KEY("<CONFIG_KEY>"), CONFIG_VALUE("<CONFIG_VALUE>"), FEATURE_NAME("<FEATURE_NAME>");
+    FILE_TYPE("<param_filetype>"),
+    URL("<param_url>"), LIST("<param_list>"), OPERATOR("<param_operator>"),
+    EXCEPTION("param_exception"),
+
+    CONFIG_KEY("param_key"), CONFIG_VALUE("param_value"),
+
+    FEATURE_NAME("param_feature"), LANGUAGE("param_language"), SMS("param_sms");
     String placeholder;
 
     AlertMessagePlaceholders(String placeholder) {
